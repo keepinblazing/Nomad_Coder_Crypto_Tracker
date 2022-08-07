77152cd7119b8e083ddb11808a0c27397ed79e91
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2022-08-07 17:37:32 +0900

    modify

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
-  background-color:${(props) => props.theme.bgColor}
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-
-
 function App() {
   return  <Container>
 
@@ -22,3 +13,9 @@ function App() {
 
 export default App;
 
+const Container = styled.div`
+  background-color:${(props) => props.theme.bgColor}
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
