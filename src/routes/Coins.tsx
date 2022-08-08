ab9042c76152547f696fd29a285f33d5cda98ab1
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2022-08-08 18:03:45 +0900

    modify

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,13 +1,45 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
 function Coins() {
+  const navigate = useNavigate();
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      
+      const json = await response.json();
+      setCoins(json.slice(0,100))
+     
+
+    })();
+    fetch("https://api.coinpaprika.com/v1/coins");
+  }, []);
+  console.log(coins)
+
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
       <CoinsList>
-        {coins.map((coin) => (<Coin key={coin.id}>{coin.name} & rarr </Coin>))}
+        {coins.map((coin) => (
+          <Coin key={coin.id} onClick={() => navigate(`/${coin.id}`)}>
+            {coin.name} &rarr;
+          </Coin>
+        ))}
       </CoinsList>
     </Container>
   );
@@ -17,65 +49,37 @@ export default Coins;
 
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
-  font-size : 48px;
+  font-size: 48px;
 `;
 
 const Container = styled.div`
-
-padding : 0px 20px;
-
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
-    height: 10vh;
-    display:flex;
-    justify-content :center;
-    align-items:center ;
-
-
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const CoinsList = styled.ul``;
 
 const Coin = styled.li`
-
-    background-color : white;
-    color : ${props => props.theme.textColor};
-    padding : 20px;
-    margin-bottom : 10px;
-    border-radius : 15px;
-
+  background-color: white;
+  color: ${(props) => props.theme.textColor};
+  padding: 20px;
+  margin-bottom: 10px;
+  border-radius: 15px;
+  a {
+    transition: color 0.5s ease-in;
+    display: block;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
 `;
-
-const coins = [
-    {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-      },
-      {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-      },
-      {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-      },
-
-
-
-]
